a7e5972caeb8d66bd92907865792b615e3cf0138
suhas-bn-1412 <52152465+suhas-bn-1412@users.noreply.github.com>
2023-11-16 00:51:54 -0800

    Code for generating Abstract syntax tree

---
+++ AST/input.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	int i, a, b;
+	
+	for(i = 0;  i < 10; i++){
+		// for(a = 1; a <= 20; a = a+2){
+			b = 25 + i;
+		// }
+	}
+
+	a = 3*9;
+
+}
---
+++ AST/output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	
+	int i, a, b;
+	
+	for(i = 0;  i < 10; i++){
+		
+			b = 25 + i;
+		
+	}
+
+	a = 3*9;
+
+}
